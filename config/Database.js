9288f2fe94e5745321fa995df369e5918dc35318
Howie Ng <hakngrow@gmail.com>
2022-11-02 08:19:05 +0800

    removed test cases

--- config/Database.js
+++ config/Database.js
@@ -9,7 +9,8 @@ const db = new Sequelize(
     process.env.DATABASE_PASSWORD, {
         host: process.env.DATABASE_HOST,
         port: process.env.DATABASE_PORT,
-        dialect: 'postgres'
+        dialect: 'postgres',
+        logging: false
     });
 
 export default db;
